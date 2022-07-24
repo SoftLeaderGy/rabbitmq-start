d5f6aaced1d458ab225b90bebd27ab1ce2ace747
SoftLeaderGy <826044698@qq.com>
2022-07-24 10:04:30 +0800

    首次提交，创建项目集成rabbitmq相关依赖

---
+++ rabbitmq-start/src/test/java/com/yang/rabbitmqstart/RabbitmqStartApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yang.rabbitmqstart;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
