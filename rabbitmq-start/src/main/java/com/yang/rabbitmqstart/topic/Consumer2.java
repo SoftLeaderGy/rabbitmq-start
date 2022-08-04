517ab85e0c09e5d96936e240a558355776c0ed6c
SoftLeaderGy <826044698@qq.com>
2022-08-04 10:17:36 +0800

    topic模型消费者

---
+++ rabbitmq-start/src/main/java/com/yang/rabbitmqstart/topic/Consumer2.java
@@ -0,0 +1,31 @@
+package com.yang.rabbitmqstart.topic;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+import com.yang.rabbitmqstart.util.RabbitMQUtils;
+
+import java.io.IOException;
+
+/**
+ * @Description: topic模型的消费者
+ * @Author: Guo.Yang
+ * @Date: 2022/08/03/21:22
+ */
+public class Consumer2 {
+    public static void main(String[] args) throws IOException {
+        Channel channel = RabbitMQUtils.getConnection().createChannel();
+        String exchangeName = "topics";
+        String routeKey = "user.#";
+        channel.exchangeDeclare(exchangeName,"topic");
+        String queue = channel.queueDeclare().getQueue();
+        channel.queueBind(queue,exchangeName,routeKey);
+        channel.basicConsume(queue,true,new DefaultConsumer(channel){
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                System.out.println(new String(body));
+            }
+        });
+    }
+}
